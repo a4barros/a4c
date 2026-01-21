825920715d38ba23da8bdafdb18f01559dda0cc6
a4barros <antenor@a4barros.com>
2026-01-20 22:27:27 -0300

    lexer and tokenizer

---
+++ a4c/Lexer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace a4c
+{
+    internal static class Lexer
+    {
+        private enum LexerStateEnum
+        {
+            NORMAL,
+            WITHIN_NUMBER,
+        }
+        public static TokenList ProcessString(string expressionStr)
+        {
+            TokenList tokens = new();
+            LexerStateEnum lexerState = LexerStateEnum.NORMAL;
+            int numberBuffer = 0;
+            foreach (char c in expressionStr + " ")
+            {
+                if (char.IsDigit(c))
+                {
+                    lexerState = LexerStateEnum.WITHIN_NUMBER;
+                    numberBuffer = numberBuffer * 10 + (c - '0');
+                    continue;
+                }
+                else
+                {
+                    lexerState = LexerStateEnum.NORMAL;
+                    if (numberBuffer != 0)
+                    {
+                        tokens.Add(TokenFactory.CreateToken(numberBuffer));
+                        numberBuffer = 0;
+                    }
+                }
+                if (lexerState == LexerStateEnum.NORMAL)
+                {
+                    Dictionary<char, TokenTypeEnum> tokenMap = new()
+                    {
+                        { '+', TokenTypeEnum.SUM },
+                        { '-', TokenTypeEnum.MINUS },
+                        { '*', TokenTypeEnum.MUL },
+                        { '/', TokenTypeEnum.DIV },
+                        { '(', TokenTypeEnum.OPEN_PARENTHESIS },
+                        { ')', TokenTypeEnum.CLOSE_PARENTHESIS },
+                    };
+
+                    tokens.Add(TokenFactory.CreateToken(tokenMap[c]));
+                }
+            }
+            return tokens;
+        }
+    }
+}
---
+++ a4c/Token.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace a4c
+{
+    public enum TokenTypeEnum
+    {
+        SUM, MINUS, MUL, DIV, OPEN_PARENTHESIS, CLOSE_PARENTHESIS, NUMBER
+    }
+    internal interface IToken
+    {
+        public bool IsNumeric();
+        public TokenTypeEnum GetTokenType();
+        public int GetValue();
+        public string ToString();
+    }
+    internal class NumericToken : IToken
+    {
+        private readonly int Value;
+        public NumericToken(int value)
+        {
+            this.Value = value;
+        }
+
+        public bool IsNumeric()
+        {
+            return true;
+        }
+
+        public TokenTypeEnum GetTokenType()
+        {
+            return TokenTypeEnum.NUMBER;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+        public override string ToString()
+        {
+            return $"<NUMBER {GetValue()}>";
+        }
+    }
+    internal class OperationToken : IToken
+    {
+        private TokenTypeEnum TokenType;
+        public OperationToken(TokenTypeEnum tokenType)
+        {
+            TokenType = tokenType;
+        }
+        public bool IsNumeric()
+        {
+            return false;
+        }
+
+        public TokenTypeEnum GetTokenType()
+        {
+            return TokenType;
+        }
+
+        public int GetValue()
+        {
+            return 0;
+        }
+        public override string ToString()
+        {
+            return $"<{GetTokenType()}>";
+        }
+    }
+    internal static class TokenFactory
+    {
+        public static IToken CreateToken(int value)
+        {
+            return new NumericToken(value);
+        }
+        public static IToken CreateToken(TokenTypeEnum tokenType)
+        {
+            return new OperationToken(tokenType);
+        }
+    }
+    internal class TokenList
+    {
+        private List<IToken> tokens = new List<IToken>();
+        public TokenList()
+        {
+        }
+        public void Add(IToken token)
+        {
+            tokens.Add(token);
+        }
+        public IToken Consume()
+        {
+            var t = tokens[0];
+            tokens.RemoveAt(0);
+            return t;
+        }
+        public override string ToString()
+        {
+            return String.Join(" ", tokens);
+        }
+    }
+}
