96221d77eb51889c8d9ce04d36bb87e53acb2c62
a4barros <antenor@a4barros.com>
2026-02-01 17:17:09 -0300

    GUI checkpoint

---
+++ a4c.UI/Display.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls;
+using Avalonia.Vulkan;
+using MsBox.Avalonia;
+
+namespace a4c.UI
+{
+    internal class Display
+    {
+        private static TextBox? DisplayBox;
+        private static Display? instance;
+        private Display(TextBox displayTextBox)
+        {
+            DisplayBox = displayTextBox;
+        }
+        public static Display GetInstance(TextBox display)
+        {
+            if (instance == null)
+            {
+                instance = new Display(display);
+            }
+            return instance;
+        }
+        public void AppendText(string text)
+        {
+            if (DisplayBox?.Text is not null && DisplayBox.Text == "0")
+            {
+                DisplayBox.Text = text;
+            }
+            else
+            {
+                DisplayBox?.Text += text;
+            }
+        }
+        public void Clear()
+        {
+            DisplayBox?.Text = "0";
+        }
+        public void Backspace()
+        {
+            if (DisplayBox?.Text is not null && DisplayBox.Text.Length > 1)
+            {
+                DisplayBox.Text = DisplayBox.Text[..^1];
+            }
+            else
+            {
+                DisplayBox.Text = "0";
+            }
+        }
+        public void Evaluate()
+        {
+            try
+            {
+                var l = Lexer.ProcessString(DisplayBox?.Text ?? "");
+                var expr = new Parser(l);
+                DisplayBox?.Text = Convert.ToString(expr.Parse().Evaluate());
+            }
+            catch (Exception ex)
+            {
+                var box = MessageBoxManager
+                    .GetMessageBoxStandard("Syntax error", ex.Message);
+
+                box.ShowAsync();
+            }
+        }
+    }
+}
--- a4c.UI/MainWindow.axaml.cs
+++ a4c.UI/MainWindow.axaml.cs
@@ -5,35 +5,22 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace a4c.UI;
 
 public partial class MainWindow : Window
 {
+    Display display;
     public MainWindow()
     {
         InitializeComponent();
-
+        display = Display.GetInstance(InputBox);
     }
-    private void Evaluate()
-    {
-        try
-        {
-            var l = Lexer.ProcessString(InputBox.Text ?? "");
-            var expr = new Parser(l);
-            InputBox.Text = Convert.ToString(expr.Parse().Evaluate());
-        }
-        catch (Exception ex)
-        {
-            var box = MessageBoxManager
-                .GetMessageBoxStandard("Syntax error", ex.Message);
-
-            box.ShowAsync();
-        }
-    }
+    
     public void Evaluate(object source, RoutedEventArgs args)
     {
-        Evaluate();
+        display.Evaluate();
     }
 
     private void Window_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
@@ -42,44 +29,115 @@ public partial class MainWindow : Window
 
         if (e.KeySymbol is null)
         {
-            return; 
+            return;
         }
-        if (operators.Contains(e.KeySymbol))
+        else if (operators.Contains(e.KeySymbol))
         {
-            if (InputBox.Text is not null && InputBox.Text == "0")
-            {
-                InputBox.Text = e.KeySymbol;
-            }
-            else
-            {
-                InputBox.Text += e.KeySymbol;
-            }
+            display.AppendText(e.KeySymbol);
         }
-        if (e.KeySymbol.All(Char.IsAsciiLetterOrDigit))
+        else if (e.KeySymbol.All(Char.IsAsciiLetterOrDigit))
         {
-            if (InputBox.Text is not null && InputBox.Text == "0")
-            {
-                InputBox.Text = e.KeySymbol;
-            }
-            else
-            {
-                InputBox.Text += e.KeySymbol;
-            }
+            display.AppendText(e.KeySymbol);
         }
-        if (e.PhysicalKey == Avalonia.Input.PhysicalKey.Backspace)
+        else if (e.PhysicalKey == Avalonia.Input.PhysicalKey.Backspace)
         {
-            if (InputBox.Text is not null && InputBox.Text.Length > 1)
-            {
-                InputBox.Text = InputBox.Text[..^1];
-            }
-            else
-            {
-                InputBox.Text = "0";
-            }
+            display.Backspace();
         }
-        if (e.PhysicalKey == Avalonia.Input.PhysicalKey.Enter || e.PhysicalKey == Avalonia.Input.PhysicalKey.NumPadEnter)
+        else if (e.PhysicalKey == Avalonia.Input.PhysicalKey.Enter || e.PhysicalKey == Avalonia.Input.PhysicalKey.NumPadEnter)
         {
-            Evaluate();
+            display.Evaluate();
         }
+    }
+    private void Button0_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("0");
+    }
+    private void Button1_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("1");
+    }
+    private void Button2_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("2");
+    }
+    private void Button3_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("3");
+    }
+    private void Button4_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("4");
+    }
+    private void Button5_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("5");
+    }
+    private void Button6_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("6");
+    }
+    private void Button7_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("7");
+    }
+    private void Button8_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("8");
+    }
+    private void Button9_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("9");
+    }
+    private void ButtonPlus_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("+");
+    }
+    private void ButtonMinus_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("-");
+    }
+    private void ButtonMulti_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("*");
+    }
+    private void ButtonDiv_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("/");
+    }
+    private void ButtonPower_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("^");
+    }
+    private void ButtonSin_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("sin(");
+    }
+    private void ButtonCos_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("cos(");
+    }
+    private void ButtonTan_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("tan(");
+    }
+    private void ButtonDecimal_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText(".");
+    }
+    private void ButtonOpenParenthesis_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText("(");
+    }
+    private void ButtonCloseParenthesis_Click(object? sender, RoutedEventArgs e)
+    {
+        display.AppendText(")");
+    }
+    private void ButtonClear_Click(object? sender, RoutedEventArgs e)
+    {
+        display.Clear();
+    }
+    private void ButtonBackspace_Click(object? sender, RoutedEventArgs e)
+    {
+        display.Backspace();
     }
 }
